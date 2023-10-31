c47a0d87c8bfa89ad124d86c59b562b0982280cf
ahmethakanbesel <ahmethakan.besel@gmail.com>
2023-10-31 19:27:45 +0300

    chore: format code

--- src/main/java/com/ahkn/springtodoapp/SpringTodoAppApplication.java
+++ src/main/java/com/ahkn/springtodoapp/SpringTodoAppApplication.java
@@ -8,9 +8,10 @@ import org.modelmapper.ModelMapper;
 @SpringBootApplication
 public class SpringTodoAppApplication {
     @Bean
-    public ModelMapper modelMapper(){
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringTodoAppApplication.class, args);
     }
